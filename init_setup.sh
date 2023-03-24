accd1fd9091df86a9f3e00f4e3442a5aa54d17dd
vivek-ck <vivek.biswas@cloudkaptan.com>
2023-03-24 11:10:39 +0530

    [CK-00] Added base framework layout

---
+++ init_setup.sh
@@ -0,0 +1,13 @@
+# Create a Python virtual environment for Selenium
+python3 -m venv selenium_env
+
+# Activate the virtual environment
+source selenium_env/bin/activate
+
+# Install SeleniumBase package within the virtual environment
+pip install seleniumbase
+
+
+#Info
+echo "#### Activate the virtual environment: source selenium_env/bin/activate ####"
+echo "#### To deactivate use : deactivate ####"
